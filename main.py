3a8b41a8df54e859266e87227409481102ade7b6
mohdahmad242 <md.ahmad0652@gmail.com>
2024-06-15 16:53:44 -0400

    Created Qatify class to organise the code

--- main.py
+++ main.py
@@ -6,91 +6,146 @@ import torchvision.transforms as transforms
 import time
 from torch.utils.data import DataLoader
 
-from model.resnet18 import resnet18
+# from model.resnet18 import resnet18
 from utils.getDataset import get_data
 from utils.train import train
 # from utils.evaluate import evaluate
 
 
 from evaluate import evaluate
+from final_qatify.qutify import Qatify
+from final_qatify.model.resnet18 import resnet18
+
+def get_qat_model(model):
+    modules_to_list = model.modules_to_fuse()
+
+    # It will keep Batchnorm
+    model.eval()
+    # fused_model = torch.ao.quantization.fuse_modules_qat(model, modules_to_list)
+
+    # This will fuse BatchNorm weights into the preceding Conv
+    fused_model = torch.ao.quantization.fuse_modules(model, modules_to_list)
+
+    # Step 3: Assign qconfigs
+    from torch.ao.quantization.fake_quantize import FakeQuantize
+    activation_qconfig = FakeQuantize.with_args(
+        observer=torch.ao.quantization.observer.HistogramObserver.with_args(
+            quant_min=0,
+            quant_max=255,
+            dtype=torch.quint8,
+            qscheme=torch.per_tensor_affine,
+        )
+    )
+
+    weight_qconfig = FakeQuantize.with_args(
+        observer=torch.ao.quantization.observer.PerChannelMinMaxObserver.with_args(
+            quant_min=-128,
+            quant_max=127,
+            dtype=torch.qint8,
+            qscheme=torch.per_channel_symmetric,
+        )
+    )
+
+    qconfig = torch.quantization.QConfig(activation=activation_qconfig,
+                                        weight=weight_qconfig)
+    fused_model.qconfig = qconfig
+
+    # Step 4: Prepare for fake-quant
+    fused_model.train()
+    fake_quant_model = torch.ao.quantization.prepare_qat(fused_model)
+
+    return fake_quant_model
+
+
+
+model = resnet18(pretrained=True)
+
+quantized_model = get_qat_model(model=model)
+
+test_bitwidth_list = [2, 4, 8, 16]
+model_size_constraints = 4
+
+qatfiy = Qatify(model, quantized_model, test_bitwidth_list, model_size_constraints)
+
+qatfiy.run()
 
 # Get the train and test loader
-dataset_name = "cifar-100" # Supported cifar-10 | cifar-100
-trainloader, testloader = get_data(dataset_name, 128)
+# dataset_name = "cifar-100" # Supported cifar-10 | cifar-100
+# trainloader, testloader = get_data(dataset_name, 128)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f'Current available device - {device}')
-model = resnet18(pretrained=True)
-model = model.to(device)
-# print(model)
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print(f'Current available device - {device}')
+# model = resnet18(pretrained=True)
+# model = model.to(device)
+# # print(model)
 
-# Step 1: architecture changes
-# QuantStubs (we will do FloatFunctionals later)
-# Done
+# # Step 1: architecture changes
+# # QuantStubs (we will do FloatFunctionals later)
+# # Done
 
-# Step 2: fuse modules (recommended but not necessary)
-modules_to_list = model.modules_to_fuse()
+# # Step 2: fuse modules (recommended but not necessary)
+# modules_to_list = model.modules_to_fuse()
 
-# It will keep Batchnorm
-model.eval()
-# fused_model = torch.ao.quantization.fuse_modules_qat(model, modules_to_list)
+# # It will keep Batchnorm
+# model.eval()
+# # fused_model = torch.ao.quantization.fuse_modules_qat(model, modules_to_list)
 
-# This will fuse BatchNorm weights into the preceding Conv
-fused_model = torch.ao.quantization.fuse_modules(model, modules_to_list)
+# # This will fuse BatchNorm weights into the preceding Conv
+# fused_model = torch.ao.quantization.fuse_modules(model, modules_to_list)
 
-# Step 3: Assign qconfigs
-from torch.ao.quantization.fake_quantize import FakeQuantize
-activation_qconfig = FakeQuantize.with_args(
-    observer=torch.ao.quantization.observer.HistogramObserver.with_args(
-        quant_min=0,
-        quant_max=255,
-        dtype=torch.quint8,
-        qscheme=torch.per_tensor_affine,
-    )
-)
+# # Step 3: Assign qconfigs
+# from torch.ao.quantization.fake_quantize import FakeQuantize
+# activation_qconfig = FakeQuantize.with_args(
+#     observer=torch.ao.quantization.observer.HistogramObserver.with_args(
+#         quant_min=0,
+#         quant_max=255,
+#         dtype=torch.quint8,
+#         qscheme=torch.per_tensor_affine,
+#     )
+# )
 
-weight_qconfig = FakeQuantize.with_args(
-    observer=torch.ao.quantization.observer.PerChannelMinMaxObserver.with_args(
-        quant_min=-128,
-        quant_max=127,
-        dtype=torch.qint8,
-        qscheme=torch.per_channel_symmetric,
-    )
-)
+# weight_qconfig = FakeQuantize.with_args(
+#     observer=torch.ao.quantization.observer.PerChannelMinMaxObserver.with_args(
+#         quant_min=-128,
+#         quant_max=127,
+#         dtype=torch.qint8,
+#         qscheme=torch.per_channel_symmetric,
+#     )
+# )
 
-qconfig = torch.quantization.QConfig(activation=activation_qconfig,
-                                      weight=weight_qconfig)
-fused_model.qconfig = qconfig
+# qconfig = torch.quantization.QConfig(activation=activation_qconfig,
+#                                       weight=weight_qconfig)
+# fused_model.qconfig = qconfig
 
-# Step 4: Prepare for fake-quant
-fused_model.train()
-fake_quant_model = torch.ao.quantization.prepare_qat(fused_model)
-
-
-
-print("\nFloat")
-evaluate(model, 'cpu')
+# # Step 4: Prepare for fake-quant
+# fused_model.train()
+# fake_quant_model = torch.ao.quantization.prepare_qat(fused_model)
 
 
-print("\nFused Model")
-evaluate(fused_model, 'cpu')
+
+# print("\nFloat")
+# evaluate(model, 'cpu')
 
 
-print("\nFake quant - PTQ")
-evaluate(fake_quant_model, 'cpu')
-
-fake_quant_model.apply(torch.ao.quantization.fake_quantize.disable_observer)
-
-print("\nFake quant - post-PTQ")
-evaluate(fake_quant_model, 'cpu')
+# print("\nFused Model")
+# evaluate(fused_model, 'cpu')
 
 
-torch.backends.quantized.engine = 'qnnpack'
-# Step 5: convert (true int8 model)
-converted_model = torch.ao.quantization.convert(fake_quant_model)
+# print("\nFake quant - PTQ")
+# evaluate(fake_quant_model, 'cpu')
 
-print("\nConverted model")
-evaluate(converted_model, 'cpu')
+# fake_quant_model.apply(torch.ao.quantization.fake_quantize.disable_observer)
+
+# print("\nFake quant - post-PTQ")
+# evaluate(fake_quant_model, 'cpu')
+
+
+# torch.backends.quantized.engine = 'qnnpack'
+# # Step 5: convert (true int8 model)
+# converted_model = torch.ao.quantization.convert(fake_quant_model)
+
+# print("\nConverted model")
+# evaluate(converted_model, 'cpu')
 
 # Define loss function and optimizer
 # criterion = nn.CrossEntropyLoss()
